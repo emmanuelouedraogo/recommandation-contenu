c28bd9e7e8b6534f239c73713e16fa28f435ed94
emmanuelouedraogo <emmanuelrhema.amjc@gmail.com>
2025-12-10 09:21:24 +0100

    flask6

--- api/function_app.py
+++ api/function_app.py
@@ -105,3 +105,4 @@ async def recommend(req: func.HttpRequest) -> func.HttpResponse:
             exc_info=True
         )
         return func.HttpResponse("Erreur interne du serveur lors de la prédiction.", status_code=500)
+    
