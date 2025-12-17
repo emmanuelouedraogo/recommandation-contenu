9210666a02db7e3008646520c902cda0f84c99bc
emmanuelouedraogo <emmanuelrhema.amjc@gmail.com>
2025-12-17 17:50:19 +0100

    update13

--- api/test_predict.py
+++ api/test_predict.py
@@ -31,6 +31,7 @@ class TestGenerateRecommendations(unittest.TestCase):
         self.mock_pipeline.recommend_items.return_value = reco_df
 
         from api.predict import Recommender
+
         # Instancier le Recommender avec les données de test en mémoire
         recommender = Recommender(pipeline=self.mock_pipeline, articles_df=self.articles_df, clicks_df=self.clicks_df)
 
@@ -52,6 +53,7 @@ class TestGenerateRecommendations(unittest.TestCase):
         # Configurer le mock pour qu'il lève une exception lors de l'appel
         self.mock_pipeline.recommend_items.side_effect = Exception("Erreur de prédiction simulée")
         from api.predict import Recommender
+
         recommender = Recommender(pipeline=self.mock_pipeline, articles_df=self.articles_df, clicks_df=self.clicks_df)
 
         recommendations = recommender.generate_recommendations(user_id=1, top_n=2)
@@ -68,6 +70,7 @@ class TestGenerateRecommendations(unittest.TestCase):
         """
         invalid_pipeline = object()  # Un objet simple qui n'a pas la méthode requise
         from api.predict import _generate_recommendations_logic
+
         recommendations = _generate_recommendations_logic(
             user_id=1, pipeline=invalid_pipeline, articles_df=self.articles_df, clicks_df=self.clicks_df
         )
@@ -79,6 +82,7 @@ class TestGenerateRecommendations(unittest.TestCase):
         """
         self.mock_pipeline.recommend_items.return_value = None  # Retourne None au lieu d'un DataFrame
         from api.predict import _generate_recommendations_logic
+
         recommendations = _generate_recommendations_logic(
             user_id=1,
             pipeline=self.mock_pipeline,
@@ -95,6 +99,7 @@ class TestGenerateRecommendations(unittest.TestCase):
         invalid_reco_df = pd.DataFrame({"id": [40], "prediction": [0.9]})
         self.mock_pipeline.recommend_items.return_value = invalid_reco_df
         from api.predict import _generate_recommendations_logic
+
         recommendations = _generate_recommendations_logic(
             user_id=1,
             pipeline=self.mock_pipeline,
